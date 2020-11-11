3112d94b7483f70019d38930430787880fe9aa33
iamrivera <a3m3rivera@gmail.com>
2020-11-10 20:49:22 -0800

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+
+  def initialize(a,b,c)
+    @side_a = a 
+    @side_b = b 
+    @side_c = c 
+  end
+
+  def kind
+    #Tests sides greater than zero  
+    non_zero = @side_a > 0 && @side_b > 0 && @side_c > 0 
+
+    #Tests triangle inequality principle met 
+    triangle_inequality = @side_a + @side_b > @side_c && @side_a + @side_c > @side_b && @side_b + @side_c > @side_a
+
+    #Tests triangle validity (expect True if conditions met)
+    valid = non_zero && triangle_inequality
+
+    #Outputs triangle kind with error message 
+    if !valid
+      raise TriangleError
+    elsif valid  && @side_a != @side_b && @side_a != @side_c && @side_b != @side_c
+        :scalene
+    elsif valid && @side_a == @side_b && @side_b == @side_c
+        :equilateral
+    elsif valid && @side_a == @side_b || @side_a == @side_c || @side_b == @side_c 
+        :isosceles
+    end
+  end
+
+
+  class TriangleError < StandardError
+  end
+
 end
